d40ad11a4ae082630cb94615e3d447e9841421f2
igorribeiro2022 <igor.rogerio.ribeiro@gmail.com>
2022-02-11 19:04:08 -0300

    Atualização 11/02/2022 - S1B35

--- JavaScript/S1B33/index.js
+++ JavaScript/S1B33/index.js
@@ -10,3 +10,11 @@ let preco = 15;
 let precoFormatado = preco.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
 
 console.log('O valor formatado fica assim: ' + precoFormatado)
+
+let cumprimento = "Bom dia"
+let nome = "Igor"
+let sobrenome = "Ribeiro"
+
+let mensagemCumprimento = (cumprimento+", "+nome+" "+sobrenome)
+
+console.log(mensagemCumprimento)
