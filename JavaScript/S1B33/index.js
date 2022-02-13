384de4227f448f84901a9e30254a7290368af988
igorribeiro2022 <igor.rogerio.ribeiro@gmail.com>
2022-02-13 12:30:39 -0300

    Atualização 13/02/2022

--- JavaScript/S1B33/index.js
+++ JavaScript/S1B33/index.js
@@ -18,3 +18,54 @@ let sobrenome = "Ribeiro"
 let mensagemCumprimento = (cumprimento+", "+nome+" "+sobrenome)
 
 console.log(mensagemCumprimento)
+
+
+let name = "Igor"
+let secondname = "Ribeiro"
+let years = 22
+console.log("Olá, meu nome é "+name+", e meu sobrenome é "+secondname+". E eu tenho "+years+" anos!")
+
+preco1 = 35 
+preco2 = 28
+
+precototal = preco1 + preco2
+
+console.log("Eu comprei uma chicara por "+preco1+" reais, e comprei dois copos por "+preco2+" reais. Tudo somou um gasto de "+precototal+" reais! Caro não?!")
+
+
+n1 = 43
+n2 = 98
+somanumber = n1 + n2
+console.log("A soma de "+n1+" + "+n2+" é "+somanumber+"!")
+
+contador = 0
+contador = contador + 1
+contador = contador + 1
+contador = contador + 1
+contador = contador + 1
+contador = contador + 1
+console.log(contador)
+
+oanoAtual = 2022
+anoNasc = 1999
+idade = oanoAtual - anoNasc
+console.log("Minha Idade é "+idade)
+
+let alturaRetangulo = 5
+let larguraRetangulo = 7
+let areaRetangulo = alturaRetangulo * larguraRetangulo
+
+console.log(areaRetangulo)
+
+let diaAtual = 11
+let mesAtual = 02
+let anoAtual = 2022
+let dataAtualFormatada = diaAtual+"/"+mesAtual+"/"+anoAtual
+
+console.log("A data de hoje é "+dataAtualFormatada)
+
+let valorA = 10
+let valorB = 20
+
+console.log(valorA)
+
---
+++ JavaScript/S1B37/index.js
@@ -0,0 +1,10 @@
+// let valor2 = "10"
+// let valor1 = 10
+
+// // // let usuario1Altura = 1.80
+// // // let usuario2Altura = 1.75
+// // // let comparacaoDeAlturas = usuario1Altura > usuario2Altura
+// // // console.log(comparacaoDeAlturas)
+
+// let comparacaoDeValores = valor1 != valor2
+// console.log(comparacaoDeValores)
