b05e4a37cdad55cb4f702d40c3b2c54267e837f8
igorribeiro2022 <igor.rogerio.ribeiro@gmail.com>
2022-02-16 20:36:40 -0300

    Final do dia 16/02/22

---
+++ JavaScript/S2A14/exercicioDois.js
@@ -0,0 +1,12 @@
+/* Agora vamos fazer um loop while usando um array */ 
+
+function mostrandoNoConsole(arr){
+    let i = 0;
+    while(i < arr.length){
+      console.log(arr[i])
+      i++
+    }
+  }
+  
+  /* insira um array e verifique o console */
+  mostrandoNoConsole(['iphone', 'imac', 'macbook', 'ipad', 'ipod', 'apple watch'])
---
+++ JavaScript/S2A14/exercicioUm.js
@@ -0,0 +1,13 @@
+// Crie um loop while que percorre por todos
+// os itens da array enquanto a variavel 'i'
+// for menor que o tamanho da array.
+
+// Imprima os itens no console
+
+let redesSociais = ['Facebook', 'Twitter', 'Instagram', 'Dev community', 'Linkedin', 'reddit', 'WhatsApp', 'Snapchat', 'Tik tok'];
+
+let i = 0;
+while (i < redesSociais.length){
+  console.log(redesSociais[i]);
+  i++;
+}
