94ed1f6c7ca4a8a44d5cd9c8a8c2bc7b11900844
igorribeiro2022 <igor.rogerio.ribeiro@gmail.com>
2022-03-20 23:43:22 -0300

    Atualização 20/03/2022

---
+++ JavaScript/DOM/S3B28/script.js
@@ -0,0 +1,6 @@
+const ul = document.createElement("ul")
+const li1 = document.createElement("li")
+li1.innerText = "repetir cada vez mais"
+ul.appendChild(li1)
+const main = document.getElementById("root")
+main.appendChild(ul)
